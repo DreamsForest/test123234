0febed34394ccdb06b6d7ad369a245da268ce082
DreamsForest <muhinv451@gmail.com>
2025-04-21 16:56:23 +0300

    teye

---
+++ python/bot_buttons.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+TOKEN = "f9LHodD0cOLX7UGehN1HCPp6Pp9mAu-ebAySBD4VVbbhY9vnNiz_DSFlvcfrHBckhjbx3WJ3-W1P32DpMDgP"  # Храните в переменных окружения Heroku!
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json
+    chat_id = data['message']['chat']['id']
+    
+    response = requests.post(
+        'https://api.max.ru/bot/v1/messages/send',
+        headers={'Authorization': f'Bearer {TOKEN}'},
+        json={
+            'chat_id': chat_id,
+            'text': 'Выберите действие:',
+            'reply_markup': {
+                'inline_keyboard': [
+                    [{'text': 'Теория', 'url': 'https://chipper-flan-72a78b.netlify.app/theory'}],
+                    [{'text': 'Задачи', 'callback_data': 'tasks'}]
+                ]
+            }
+        }
+    )
+    return jsonify({'status': 'ok'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
